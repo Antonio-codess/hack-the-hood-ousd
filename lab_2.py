e4b68432a7feef112aca4b7698c547aabe491126
antonio gonzalez ohoa <ant@antonios-MacBook-Pro.local>
2025-06-17 14:30:31 -0700

    completed lab2

---
+++ lab_2.py
@@ -0,0 +1,6 @@
+name = "antonio"
+nikname = "ant"
+age = 15
+use_py = False
+hobbies = ["reading","games","shows"]
+favorite_things = {"tv shows":"higurashi wen they cry","color": "mint green","games":"deltaruin" }
